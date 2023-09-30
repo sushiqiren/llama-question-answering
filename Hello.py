7d1de8d295924c7d477bf59cab8389482125c56f
sushiqiren <100112269+sushiqiren@users.noreply.github.com>
2023-09-30 23:42:14 +0000

    change the Hello.py reset button

--- Hello.py
+++ Hello.py
@@ -42,10 +42,10 @@ if get_button:
     st.session_state.input_values["answer"] = res['answer']
 
 # Handle reset button click
-if reset_button:
-    question_input = ""
-    context_input = ""    
+if reset_button:          
     st.session_state.input_values = {"question": "", "context": "", "answer": ""}
+    question_input = st.session_state.input_values["question"]
+    context_input = st.session_state.input_values["context"] 
 
 # Update the input values when the user enters text
 if not reset_button:  # Only update when the reset button is not clicked
