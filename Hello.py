f5862d8f146e865a4f6f0b0ca2efa3176b420140
sushiqiren <100112269+sushiqiren@users.noreply.github.com>
2023-10-01 00:51:34 +0000

    Make some changes to add functions to avoid memory leakage

--- Hello.py
+++ Hello.py
@@ -4,47 +4,61 @@ from transformers import (
     AutoModelForQuestionAnswering,
     AutoTokenizer,    
 )
+import gc
 
+# Enable garbage collection
+gc.enable()
 
 st.title("Question Answering Workflow for Swinburne Online FAQs")
 
-model_name = "hzsushiqiren/bert-finetuned-squad"
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+def deploy():
 
-nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+    # Add text inputs for question and context
+    question_input = st.text_input("Question:")
+    context_input = st.text_area("Context:")
 
-# # Use session state to manage input values
-# if "input_values" not in st.session_state:
-#     st.session_state.input_values = {"question": "", "context": "", "answer": ""}
+    # Create a button for get answers
+    get_button = st.button("Get Answer")
 
-# Add text inputs for question and context
-question_input = st.text_input("Question:")
-context_input = st.text_area("Context:")
-
-
-# Create a button for get answers
-get_button = st.button("Get Answer")
-
-# Create a button for resetting inputs
-reset_button = st.button("Clear Answer")
-
-if get_button:
-    QA_input = {
-        'question': question_input,
-        'context': context_input
-    }
-
-    res = nlp(QA_input)
-
-    st.text_area("Answer:", res['answer'])
-    st.write("Score:", res['score'])
+    # Create a button for resetting inputs
+    reset_button = st.button("Clear Answer")
     
+    model_name = "hzsushiqiren/bert-finetuned-squad"
+    model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-# Handle reset button click
-if reset_button:
-    question_input = ""
-    context_input = "" 
+    nlp = pipeline('question-answering', model=model, tokenizer=tokenizer)
+
+    # # Use session state to manage input values
+    # if "input_values" not in st.session_state:
+    #     st.session_state.input_values = {"question": "", "context": "", "answer": ""}
+
+    if get_button:
+        QA_input = {
+            'question': question_input,
+            'context': context_input
+        }
+
+        res = nlp(QA_input)
+
+        st.text_area("Answer:", res['answer'])
+        st.write("Score:", res['score'])
+        
+
+    # Handle reset button click
+    if reset_button:
+        question_input = ""
+        context_input = ""
+
+    del (
+        model_name,
+        model,
+        tokenizer,
+        nlp,        
+    )
+    gc.collect()
+
+deploy() 
 
 # # Update the input values when the user enters text
 # if not reset_button:  # Only update when the reset button is not clicked
